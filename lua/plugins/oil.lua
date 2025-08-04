d101b908c97bc3547e6160829d702e7dbc421532
Mozzie <donytiara@gmail.com>
2025-08-04 21:09:33 +0700

    oil indent

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,18 +1,18 @@
 return {
-  enable = false,
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  keys = {
-    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" }
-  },
-  config = function()
-    require('oil').setup()
-  end,
-  -- Optional dependencies
-  -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+	enable = false,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	keys = {
+		{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+	},
+	config = function()
+		require("oil").setup()
+	end,
+	-- Optional dependencies
+	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
 }
