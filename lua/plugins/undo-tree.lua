aaba315bc7f45aae20ad5bb6bb64bef14b7a7043
Mozzie <donytiara@gmail.com>
2025-08-04 21:17:52 +0700

    indent

--- lua/plugins/undo-tree.lua
+++ lua/plugins/undo-tree.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    'mbbill/undotree',
-    config = function()
-      -- Enable persistent undo
-      vim.opt.undofile = true
-      vim.opt.undodir = vim.fn.expand('~/.vim/undodir')
-      
-      -- Create undodir if it doesn't exist
-      local undodir = vim.fn.expand('~/.vim/undodir')
-      if vim.fn.isdirectory(undodir) == 0 then
-        vim.fn.mkdir(undodir, 'p')
-      end
-      
-      -- Create global function for undotree toggle
-      _G.undotree_toggle = function()
-        vim.cmd.UndotreeToggle()
-      end
-    end,
-  }
+	{
+		"mbbill/undotree",
+		config = function()
+			-- Enable persistent undo
+			vim.opt.undofile = true
+			vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
+
+			-- Create undodir if it doesn't exist
+			local undodir = vim.fn.expand("~/.vim/undodir")
+			if vim.fn.isdirectory(undodir) == 0 then
+				vim.fn.mkdir(undodir, "p")
+			end
+
+			-- Create global function for undotree toggle
+			_G.undotree_toggle = function()
+				vim.cmd.UndotreeToggle()
+			end
+		end,
+	},
 }
