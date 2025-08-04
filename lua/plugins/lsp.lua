ad34e76d0c7f976edbf3817a638a492ac4705f42
Mozzie <donytiara@gmail.com>
2025-08-04 20:24:27 +0700

    indent

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,63 +1,63 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      'saghen/blink.cmp',
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-     },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"saghen/blink.cmp",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
 
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      -- Common on_attach function for all LSP servers
-      local on_attach = function(client, bufnr)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
-      end
+		config = function()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			-- Common on_attach function for all LSP servers
+			local on_attach = function(client, bufnr)
+				-- Enable completion triggered by <c-x><c-o>
+				vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+			end
 
-      -- Setup language servers
-      local lspconfig = require('lspconfig')
-      -- Lua Language Server
-      lspconfig.lua_ls.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          Lua = {
-            runtime = {
-              version = 'LuaJIT',
-            },
-            diagnostics = {
-              globals = {'vim'},
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            telemetry = {
-              enable = false,
-            },
-          },
-        },
-      }
+			-- Setup language servers
+			local lspconfig = require("lspconfig")
+			-- Lua Language Server
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
 
-      -- C/C++ Language Server (clangd)
-      lspconfig.clangd.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        cmd = { "clangd", "--background-index" },
-        filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-      }
+			-- C/C++ Language Server (clangd)
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				cmd = { "clangd", "--background-index" },
+				filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+			})
 
-      -- Add more language servers as needed
-      -- Example for Python:
-      -- lspconfig.pyright.setup {
-      --   capabilities = capabilities,
-      --   on_attach = on_attach,
-      -- }
-      -- Example for TypeScript/JavaScript:
-      -- lspconfig.ts_ls.setup {
-      --   capabilities = capabilities,
-      --   on_attach = on_attach,
-      -- }
-    end
-  }
+			-- Add more language servers as needed
+			-- Example for Python:
+			-- lspconfig.pyright.setup {
+			--   capabilities = capabilities,
+			--   on_attach = on_attach,
+			-- }
+			-- Example for TypeScript/JavaScript:
+			-- lspconfig.ts_ls.setup {
+			--   capabilities = capabilities,
+			--   on_attach = on_attach,
+			-- }
+		end,
+	},
 }
