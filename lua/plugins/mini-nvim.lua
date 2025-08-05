78c09132a8679277f1bc3deb4528cf18918a2d16
Mozzie <donytiara@gmail.com>
2025-08-06 01:17:53 +0700

    hmmmm yazi and telescope thinkge

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -1,10 +1,11 @@
 return {
-	'echasnovski/mini.nvim', version = '*',
-	config = function ()
-		require('mini.pairs').setup()
-		require('mini.misc').setup()
-		
+	"echasnovski/mini.nvim",
+	version = "*",
+	config = function()
+		require("mini.pairs").setup()
+		require("mini.misc").setup()
+
 		-- Auto root to the directory of the current buffer
-		require('mini.misc').setup_auto_root()
-	end
+		require("mini.misc").setup_auto_root()
+	end,
 }
