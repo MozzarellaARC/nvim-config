8755395dd91c2dd48c278e879a8c79a33585929b
Mozzie <donytiara@gmail.com>
2025-10-14 07:33:59 +0700

    OK

---
+++ lua/plugins/fidget.lua
@@ -0,0 +1,32 @@
+return {
+	"j-hui/fidget.nvim",
+	opts = {
+		notification = { override_vim_notify = true },
+	},
+
+	config = function(_, opts)
+		local fidget = require("fidget")
+		fidget.setup(opts)
+		local notify = require("fidget.notification").notify
+
+		local function fidget_message(action, file)
+			local file_short = vim.fn.fnamemodify(file, ":p:.")
+			local lines = vim.api.nvim_buf_line_count(0)
+			local bytes = vim.fn.getfsize(file)
+			local msg = string.format('"%s" %dL, %dB %s', file_short, lines, bytes, action)
+			notify(msg, vim.log.levels.INFO, { title = "File Event", ttl = 3 })
+		end
+
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			callback = function(args)
+				fidget_message("written", args.file)
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("BufDelete", {
+			callback = function(args)
+				fidget_message("buffer removed", args.file)
+			end,
+		})
+	end,
+}
