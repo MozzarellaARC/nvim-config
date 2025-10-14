8192823a9a52885f5b62d0d646af364ea66c9885
Mozzie <donytiara@gmail.com>
2025-10-14 07:39:26 +0700

    truncate home with tilde

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -8,9 +8,8 @@ return {
 		local fidget = require("fidget")
 		fidget.setup(opts)
 		local notify = require("fidget.notification").notify
-
 		local function fidget_message(action, file)
-			local file_short = vim.fn.fnamemodify(file, ":p:.")
+			local file_short = vim.fn.fnamemodify(file, ":~")
 			local lines = vim.api.nvim_buf_line_count(0)
 			local bytes = vim.fn.getfsize(file)
 			local msg = string.format('"%s" %dL, %dB %s', file_short, lines, bytes, action)
