66886a9b47faf3cf723ba9a4c1f765a454c68294
Mozzie <donytiara@gmail.com>
2025-10-14 09:48:20 +0700

    test

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -8,15 +8,12 @@ return {
 	config = function(_, opts)
 		require("fidget").setup(opts)
 		local notify = require("fidget.notification").notify
+
+		-- shorten path (replace home with ~)
 		local function shorten(path)
 			local home = vim.loop.os_homedir()
 			return path:gsub("^" .. vim.pesc(home), "~")
 		end
-
-		-- Suppress stdout/stderr completely:
-		vim.api.nvim_create_user_command("GitPushSilent", function()
-			git_async({ "git", "push", "--quiet" }, "Git push complete")
-		end, {})
 
 		----------------------------------------------------------------
 		-- File events
@@ -25,29 +22,39 @@ return {
 			callback = function(args)
 				local lines = vim.fn.line("$")
 				local bytes = vim.fn.getfsize(args.file)
-				notify(string.format('"%s" %dL, %dB written', shorten(args.file), lines, bytes), vim.log.levels.INFO)
+				vim.schedule(function()
+					notify(
+						string.format('"%s" %dL, %dB written', shorten(args.file), lines, bytes),
+						vim.log.levels.INFO
+					)
+				end)
 			end,
 		})
 
-		local notify = require("fidget.notification").notify
-
+		----------------------------------------------------------------
+		-- Git commands
+		----------------------------------------------------------------
 		vim.api.nvim_create_user_command("GitCommit", function(args)
 			vim.system({ "git", "commit", "-am", args.args }, { text = true }, function(res)
-				if res.code == 0 then
-					notify("Git commit complete ✓", vim.log.levels.INFO, { title = "Git" })
-				else
-					notify("Git commit failed", vim.log.levels.ERROR, { title = "Git" })
-				end
+				vim.schedule(function()
+					if res.code == 0 then
+						notify("Git commit complete ✓", vim.log.levels.INFO, { title = "Git" })
+					else
+						notify("Git commit failed:\n" .. res.stderr, vim.log.levels.ERROR, { title = "Git" })
+					end
+				end)
 			end)
 		end, { nargs = 1 })
 
 		vim.api.nvim_create_user_command("GitPush", function()
 			vim.system({ "git", "push" }, { text = true }, function(res)
-				if res.code == 0 then
-					notify("Git push complete ✓", vim.log.levels.INFO, { title = "Git" })
-				else
-					notify("Git push failed", vim.log.levels.ERROR, { title = "Git" })
-				end
+				vim.schedule(function()
+					if res.code == 0 then
+						notify("Git push complete ✓", vim.log.levels.INFO, { title = "Git" })
+					else
+						notify("Git push failed:\n" .. res.stderr, vim.log.levels.ERROR, { title = "Git" })
+					end
+				end)
 			end)
 		end, {})
 	end,
