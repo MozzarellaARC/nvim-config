169096e9a6d67cf5c98f9c5d0f9bd254afc51586
Mozzie <mozzarellah.g@gmail.com>
2025-10-17 22:37:33 +0700

    pwsh fix

--- lsp/pwsh.lua
+++ lsp/pwsh.lua
@@ -1,6 +1,6 @@
 return {
-	vim.lsp.config("pwsh", {
-		cmd = { "pwsh", "-NoLogo", "-NoProfile", "-Command", "c:/PSES/Start-EditorServices.ps1 ..." },
+	vim.lsp.config("powershell_es", {
+		cmd = { "powershell_es", "-NoLogo", "-NoProfile", "-Command", "c:/PSES/Start-EditorServices.ps1 ..." },
 		bundle_path = "c:/w/PowerShellEditorServices",
 		root_markers = { "PSScriptAnalyzerSettings.psd1", ".git" },
 		shell = "powershell.exe",
